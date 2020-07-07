92300ec376b08cdce9fe134a0e2e09fa8c5a5918
erniuxa <ernestonas@gmail.com>
2020-07-07 11:11:51 +0300

    Splash screen delay

--- app/src/main/java/com/example/taxi/SplashScreen.java
+++ app/src/main/java/com/example/taxi/SplashScreen.java
@@ -1,17 +1,42 @@
 package com.example.taxi;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends Activity {
+
+    private Handler mWaitHandler = new Handler();
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
 
-        startActivity(new Intent(this, MainActivity.class));
+        mWaitHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    //Go to next page i.e, start the next activity.
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    // Baigti Splash Activity kai nenorime kad rodytu paspaudziant back mygtuka
+                    finish();
+                } catch (Exception ignored) {
+                    ignored.printStackTrace();
+                }
+            }
+        }, 5000);  // 5 sekundziu uzdelsimas
     }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mWaitHandler.removeCallbacksAndMessages(null);
+    }
+
+
 }
 
 
